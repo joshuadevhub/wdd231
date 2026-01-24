f79b4680e1be1c9333ff871c784f7621a571a216
joshuadevhub <elemide.j.dev@gmail.com>
2026-01-24 02:52:44 +0100

    Implement dynamic display of filtered chamber members including logo, contact info, and membership level

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -8,8 +8,8 @@ const currentSunrise = document.querySelector("#sunrise");
 const currentSunset = document.querySelector("#sunset");
 const weatherIcon = document.querySelector("#weather-icon");
 
-
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=6.65&lon=3.27&units=metric&appid=0a2d342572fc7a9b609e7bd50e04c020";
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=6.65&lon=3.27&units=metric&appid=0a2d342572fc7a9b609e7bd50e04c020";
 
 const getCurrentWeather = async () => {
   try {
@@ -27,7 +27,6 @@ const getCurrentWeather = async () => {
 };
 
 function displayData(data) {
-
   // Populate HTML tag
   currentTemp.innerHTML = `<b>${data.main.temp}&deg;C</b>`;
   currentDesc.textContent = `${data.weather[0].description}`;
@@ -46,19 +45,19 @@ function displayData(data) {
 
   const options = {
     hour: "2-digit",
-    minute: "2-digit"
+    minute: "2-digit",
   };
 
   const sunriseTime = sunrise.toLocaleTimeString([], options);
   const sunsetTime = sunset.toLocaleTimeString([], options);
 
-  currentSunrise.textContent = `Sunrise: ${sunriseTime}`
+  currentSunrise.textContent = `Sunrise: ${sunriseTime}`;
   currentSunset.textContent = `Sunset: ${sunsetTime}`;
-
 }
 
 // javaScript For The Weather Forecast
-const foreCastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=6.65&lon=3.27&units=metric&appid=0a2d342572fc7a9b609e7bd50e04c020";
+const foreCastUrl =
+  "https://api.openweathermap.org/data/2.5/forecast?lat=6.65&lon=3.27&units=metric&appid=0a2d342572fc7a9b609e7bd50e04c020";
 
 const getThreeDayForecast = async () => {
   try {
@@ -72,7 +71,7 @@ const getThreeDayForecast = async () => {
   } catch (error) {
     console.log("Error Fetching Data:", error.message);
   }
-}
+};
 
 // Function to get 3-day forecast
 function displayForeCast(data) {
@@ -94,7 +93,7 @@ function displayForeCast(data) {
 
   // Display in HTML
   const foreCastContainer = document.getElementById("forecast-days");
-  foreCastContainer.innerHTML = '';
+  foreCastContainer.innerHTML = "";
 
   nextThreeDays.forEach((date) => {
     // Pick Midday Forecast if Available
@@ -115,8 +114,104 @@ function displayForeCast(data) {
     dayElement.innerHTML = `${dayName}: <b>${temperature}&deg;C</b>`;
 
     foreCastContainer.appendChild(dayElement);
-  })
+  });
+}
+
+// javaScript to display Companies Spotlight
+const companyData = "../chamber/data/members.json";
+
+const fetchCompanyData = async () => {
+  try {
+    const response = await fetch(companyData);
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(errorMessage);
+    }
+    const data = await response.json();
+    displayCompanyData(data.companies);
+  } catch (error) {
+    console.log("Error Fetching Data", error.message);
+  }
+};
+
+function displayCompanyData(companies) {
+  const filteredCompany = companies.filter((company) => {
+    return company.membershipLevel === 2 || company.membershipLevel === 3;
+  });
+
+  const membersToShow = [];
+  while (membersToShow.length < 3 && filteredCompany.length > 0) {
+    const randomIndex = Math.floor(Math.random() * filteredCompany.length);
+    const member = filteredCompany[randomIndex];
+    membersToShow.push(member);
+    filteredCompany.splice(randomIndex, 1);
+  }
+
+  // Display in HTML
+  const businessProfile = document.querySelector(".business-profile");
+  businessProfile.innerHTML = "";
+
+  membersToShow.forEach((member) => {
+    const businessSection = document.createElement("section");
+    businessSection.classList.add("business-section");
+
+    const businessHeading = document.createElement("div");
+    businessHeading.classList.add("business-heading");
+
+    const businessNameSpan = document.createElement("span");
+    const tagLineSpan = document.createElement("p");
+
+    const businessInfo = document.createElement("div");
+    businessInfo.classList.add("business-info");
+
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("images");
+
+    const businessLogo = document.createElement("img");
+
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("info");
+
+    const emailSpan = document.createElement("span");
+    const phoneSpan = document.createElement("span");
+
+    const urlSpan = document.createElement("span");
+    // Remove the https:// from the url
+    const splitUrl = member.website.split("//")[1];
+
+    const memberShipSpan = document.createElement("span");
+
+    businessNameSpan.textContent = `${member.name}`;
+    tagLineSpan.textContent = `${member.tagline}`;
+    businessLogo.setAttribute("src", `${member.image}`);
+    businessLogo.setAttribute("alt", `${member.name} Image`);
+    businessLogo.setAttribute("loading", "lazy");
+    businessLogo.width = 100;
+    businessLogo.height = 100;
+    emailSpan.innerHTML = `<b>Email:</b> ${member.email}`;
+    phoneSpan.innerHTML = `<b>Phone:</b> ${member.phone}`;
+    urlSpan.innerHTML = `<b>URL:</b> ${splitUrl}`;
+
+    let membershipText;
+    if (member.membershipLevel === 3) {
+      membershipText = "Gold";
+    } else if (member.membershipLevel === 2) {
+      membershipText = "Silver";
+    }
+    memberShipSpan.innerHTML = `<b>Membership:</b> ${membershipText}`;
+
+    businessHeading.append(businessNameSpan, tagLineSpan);
+    businessSection.appendChild(businessHeading);
+    imageDiv.appendChild(businessLogo);
+    businessInfo.appendChild(imageDiv);
+    infoDiv.append(emailSpan, phoneSpan, urlSpan, memberShipSpan);
+    businessInfo.appendChild(infoDiv);
+    businessSection.appendChild(businessInfo)
+
+    businessProfile.appendChild(businessSection);
+  });
 }
 
 getCurrentWeather();
 getThreeDayForecast();
+fetchCompanyData();
